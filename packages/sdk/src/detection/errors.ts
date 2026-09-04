380fe6831f5ede6ad564485ca28da9c60369ab40
WelloBuilds <walidmoussa00@gmail.com>
2026-09-04 20:04:06 +0300

    feat: Add unhandled rejection detection and enhance error logging in SDK

--- packages/sdk/src/detection/errors.ts
+++ packages/sdk/src/detection/errors.ts
@@ -14,6 +14,27 @@ export const errors = () => {
             payload: errorPayload,
             createdAt: new Date()
         }
-        console.log("error" , feedbackEvent)
+        console.log(feedbackEvent)
+    })
+    window.addEventListener('unhandledrejection' , (e)=>{
+
+        const reason = e.reason;
+        const message = reason instanceof Error 
+            ? reason.message
+            : typeof reason === "string"
+              ? reason
+              : "Unhandled rejection";
+
+        const errorPayload: ErrorPayload = {
+            type: 'unhandledRejection',
+            message: message,
+        }
+        const feedbackEvent: Event = {
+            id: crypto.randomUUID(),
+            type: EventType.error,
+            payload: errorPayload,
+            createdAt: new Date()
+        }
+        console.log(feedbackEvent)
     })
 }
